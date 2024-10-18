31df8610ef12e95aac48c1ba3e9044fc48c13698
Jonathas <jonathass5678@gmail.com>
2024-10-17 23:06:42 -0300

    techfix_web

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect, ReactNode } from "react";
+import { createContext, useState, ReactNode } from "react";
 import { setCookie } from "nookies";
 import { useNavigate } from "react-router-dom";
 import { api } from "../services/api";
